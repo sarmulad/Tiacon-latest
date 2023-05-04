9160095a2f40943546deafbd01d9705fa9772dc4
sarmulad <resolvetech2020@gmail.com>
2023-05-04 15:58:47 +0100

    form fix2

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -6,12 +6,16 @@ type FormValues = {
   };
 
 const NewsLetter = ()=>{
-    const [formValues, setFormValues] = useState<FormValues>({email: '',});
+    
+      const [formValues, setFormValues] = useState<FormValues>({
+        email: '',
+      });
       const [isSubmitting, setIsSubmitting] = useState(false);
       const [isSubmitted, setIsSubmitted] = useState(false);
     
       const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+    
         setIsSubmitting(true);
     
         try {
@@ -19,9 +23,9 @@ const NewsLetter = ()=>{
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams({
-                'form-name': 'Newsletter', // This is the name of your Netlify form
-                ...formValues,
-              }).toString(),
+              'form-name': 'newsletter', // This is the name of your Netlify form
+              ...formValues,
+            }).toString(),
           });
     
           if (response.ok) {
@@ -36,7 +40,7 @@ const NewsLetter = ()=>{
         setIsSubmitting(false);
       };
     
-      const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFormValues({ ...formValues, [e.target.name]: e.target.value });
       };
 
@@ -45,10 +49,16 @@ const NewsLetter = ()=>{
             <div className="bg-[#0D1631] rounded-[48px] w-full md:max-w-[956px] h-[400px] md:max-h-[348px] p-5 md:p-14 flex flex-col justify-center items-center ">
                 <h4 className="text-[24px] mb-3 md:text-[48px] font-bold text-[#fff]">Sign Up for Newsletter</h4>
                 <p className="text-[12px] md:text-[16px] text-[#D9D9D9] font-medium text-center leading-[22px]">Stay up-to-date with Tia, subscribe to our newsletter</p>
-                <form className="mt-[40px] w-[95%] md:w-[80%] bg-[transparent]" onSubmit={handleSubmit}   data-netlify="true" >
+                <form 
+                    className="mt-[40px] w-[95%] md:w-[80%] bg-[transparent]" 
+                    onSubmit={handleSubmit}   
+                    data-netlify="true"
+                    name='newsletter'
+                 >
+                    <input type="hidden" name="form-name" value="newsletter" />
+
                     <div className="gradient-button p-[2px] h-[72px]  rounded-[64px] flex items-center">
                      <div className="relative rounded-[64px] flex bg-[#0D1631] w-full">
-                        <input type="hidden" name="form-name" value="Newsletter" />
                         <input   
                         type="email" 
                         value={formValues.email} 
